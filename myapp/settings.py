11c32bd37870703f4844847aac9da6d4311f5978
hoonmok.moon <hoonmok.moon@gmail.com>
2023-01-12 00:49:24 +0900

    mariadb

--- myapp/settings.py
+++ myapp/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'candle',
+        'USER': 'candle_user',
+        'PASSWORD': SECRET_KEY,#'mypassword',
+        'HOST': 'mariadb',
+        'PORT': '3306',
     }
 }
 
