80e24c4ac021db6931ada26e776b7eea3347e5dd
hoonmok.moon <hoonmok.moon@gmail.com>
2023-01-12 02:13:13 +0900

    candle model - list default string

--- candle/models.py
+++ candle/models.py
@@ -5,5 +5,9 @@ class Candle(models.Model):
     no = models.indexes
     title = models.CharField(max_length=255)
     content = models.TextField()
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # default string(for list)
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
