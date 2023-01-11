0cf908b239f92ab4f63cc3bad557be82463c8645
hoonmok.moon <hoonmok.moon@gmail.com>
2023-01-12 01:40:26 +0900

    candle model

--- candle/models.py
+++ candle/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Candle(models.Model):
+    no = models.indexes
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
